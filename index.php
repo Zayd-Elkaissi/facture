7a26dfaa9d4210587c656331f47b9772bf7664a1
zaydelkassi <92026156+zaydelkassi@users.noreply.github.com>
2022-02-02 16:25:32 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
                 <img href="#" src="assets/images/logo-removebg-preview.png">
             </div>
             <div class="main d-flex flex-column justify-content-center " >
-                <form name="form" action="" method="$_GET">
+                <form action="script.php" method="post">
                     
                     <input type="text" class="form-control" name="oldindex" id="oldindex" placeholder="s'il vous plait entrer la ancienne index"> <br>
                     <input type="text" class="form-control" name="newindex" id="newindex" placeholder="s'il vous plait entrer la nouvelle index"> <br>
@@ -31,127 +31,14 @@
                         <option value="15-20">15-20</option>
                         <option value="3>0">>30</option>
                     </select>
-                    <button type="button" class="d-flex justify-content-center btn btn-dark">calculer</button>
+                    <button type="submit" class="d-flex justify-content-center btn btn-dark">calculer</button>
                 </form>
             </div>
         </div>
-        <div class="px-0"><table class="table table-bordered">
-            <thead >
-                <tr class="">
-                    <th scope="col">Ancien index :</th>
-                    <th scope="col">Nouvel index :</th>
-                    <th scope="col">Consommation :</th>
-                </tr>
-            </thead>
-        </table></div>
-
-        <section class="">
-            <div class="container">
-              <div class="row">
-                <div class="col">
-
-        <!-- col1 -->
-        <div class="pt-5 mt-3">
-            <h6>CONSOMMATION ELECTRICITE</h6>
-        <h6 class="text-secondary small ">TRANCHE 1</h6>
-        <h6 class="text-secondary small pt-1">TRANCHE 2</h6>
-        <h6 class="py-2 ">REDEVANCE FIXE ELECTRICITE</h6>
-        <h6 class="pb-1">TAXES POUR LE COMPTE DE L’ETAT</h6>
-        <h6 class="text-secondary small pt-1">TOTAL TVA</h6>
-        <h6 class="text-secondary small pt-2 ">TIMBRE</h6>
-        <h6 class="pt-1">SOUS-TOTAL</h6><br><br>
-        <h6>TOTAL ÉLECTRICITÉ</h6>
-    </div></div><br>
-
-    <!-- col2 -->
-
-    <div class="col-6"><table  class="table text-center border-white ">
-        <thead class="">
-          <tr>
-            <th scope="col">مفوتر<br>Facturé</th>
-            <th scope="col">س.و<br>P.U</th>
-            <th scope="col">المبلغ د.إ.ر<br>Montant HT</th>
-            <th scope="col">ض.ق.م<br>Taux TVA</th>
-                <th>مبلغ الرسوم<br>Montant Taxes</th>
-          </tr>
-        </thead> <tbody class="border-white"><tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td class="pt-3">14%</td>
-            <td></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td class="pt-0">14%</td>
-            <td></td>
-        </tr>
-        <tr><td> </td><td> </td><td> </td><td> </td> <td> </td> </tr>
-        <tr><td> </td><td> </td><td> </td><td> </td> <td> </td> </tr>
-        <tr><td> </td><td> </td><td> </td><td> </td> <td> </td> </tr>
-        <tr><td> </td><td> </td><td> </td><td> </td> <td> </td> </tr>
-        <tr><td> </td><td> </td><td> </td><td> </td> <td> </td> </tr>
-        <tr><td> </td><td> </td><td> </td><td> </td> <td> </td> </tr>
-        <tr><td> </td><td> </td><td> </td><td> </td> <td> 0,45</td> </tr>
-        </tr>
-    </tbody></table></div>
-
-    <!-- col3 -->
-    <div class="col pt-5 mt-3 text-end">
-        <h6>إستھلاك الكھرباء</h6>
-        <h6 class="text-secondary small pt-1">1الشطر</h6>
-        <h6 class="text-secondary small pt-1">2الشطر</h6>
-        <h6 class="py-2">إثاوة ثابتة الكھرباء</h6>
-        <h6 class="pb-1"> الرسوم المؤداة لفائدة الدولة </h6>
-        <h6 class="text-secondary small pt-1">مجموع ض.ق.م</h6>
-        <h6 class="text-secondary small pt-2">الطابع</h6>
-        <h6 class="bt-1">المجموع الجزئي</h6><br><br>
-        <h6>
-            مجموع الكھرباء</h6>
-        </div>
-              </div>
-            </div>
-        </section>
-        
-        
     </main>
     
-    <!-- <?php
-            if(array_key_exists('button', $_POST)) {
-                facture();
-            }
-    echo "\nhello world";
-    $oldindex=$_GET['oldindex'];
-    $newindex=$_GET['newindex'];
-    $caliber=$_GET['caliber'];
-function facture(){
-    global $caliber, $newindex, $oldindex;
-    echo '<h3>'.$oldindex.'</h3>';
-    echo '<h3>'.$newindex.'</h3>';
-    echo '<h3>'.$caliber.'</h3>';
-    echo "<script>console.log(1231231231231);</script>"
-}
 
-    ?> -->
-
-      <!-- Newsletter
-      <section class="bg-primary text-light p-5">
-        <div class="container">
-          <div class="d-md-flex justify-content-between align-items-center">
-            <h3 class="mb-3 mb-md-0">Sign Up For Our Newsletter</h3>
-  
-            <div class="input-group news-input">
-              <input type="text" class="form-control" placeholder="Enter Email" />
-              <button class="btn btn-dark btn-lg" type="button">Submit</button>
-            </div>
-          </div>
-        </div>
-      </section>
-   -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
 </body>
 
 </html>
--- script.php
+++ script.php
@@ -1,49 +1,166 @@
 <?php
-function facture($oldindex, $newindex){
     $oldindex=$_POST['oldindex'];
     $newindex=$_POST['newindex'];
+    $caliber = filter_input(INPUT_POST, 'caliber', FILTER_SANITIZE_STRING);
+    $redevance = 0;
     $kwh= $newindex - $oldindex;
     $tranche='';
     $P_U=0;
     $timbre = 0.45; 
     $montantht = 0;
-    $tva= 0.14;
-    $mttaxes = $montantht * $tva;
-
-            if($kwh <= 150){
+    $tva= 0.14; 
+    echo '<script>console.log('.$caliber.');</script>';
+    
+    if($kwh <= 150){
                 if($kwh > 0 && $kwh <= 100){
                     $tranche = 'TRANCHE1';
                     $P_U = 0.794;
-                    echo $tranche;
-                }elseif($kwh>100){
+                    echo '<script>console.log('.$P_U.');</script>';
+                }else{
                     $tranche = 'TRANCHE2';
                     $P_U = 0.883;
-                    echo $tranche;
+                    echo '<script>console.log('.$P_U.');</script>';
                 }
-
-            }elseif($kwh >150){
-                if($kwh>150 && $kwh<= 120){
+                
+            }else{
+                if($kwh<= 210){
                     $tranche = 'TRANCHE3';
                     $P_U = 0.9451;
-                    echo $tranche;
-                }elseif($kwh>210 && $kwh <= 310){
+                    echo '<script>console.log('.$P_U.');</script>';
+                }elseif($kwh <= 310){
                     $tranche = 'TRANCHE4';
                     $P_U = 1.0489;
-                    echo $tranche;
-                }elseif($kwh>310 && $kwh<=510){
+                    echo '<script>console.log('.$P_U.');</script>';
+                }elseif($kwh<=510){
                     $tranche = 'TRANCHE5';
                     $P_U = 1.2915;
-                    echo $tranche;
+                    echo '<script>console.log('.$P_U.');</script>';
                 }elseif($kwh>510){
                     $tranche = 'TRANCHE6';
                     $P_U = 1.4975;
-                    echo $tranche;
+                    echo '<script>console.log('.$P_U.');</script>';
                 }
             }
+            $montantht = $kwh * $P_U;
+            $mttaxes = $montantht * $tva;
+            $redevancetx = $redevance * $tva;
+
+            if($caliber == -5   ){
+                echo '<h1>hello world</h1>';
+            }else{
+                echo '<h1>hellsdasdasdo world</h1>';
+                
+            }
             
-        
-    }
+            
 
-    
+?>
 
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- main CSS -->
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SoliDao</title>
+</head>
+
+        <div class="px-0">
+            <table class="table table-bordered">
+            <thead >
+                <tr class="">
+                    <th scope="col"><td>Ancien index:</td> <td><?php echo ' '.$oldindex;?></td></th>
+                    <th scope="col"><td>Nouvelle index:</td> <td><?php echo ' '.$newindex;?></td></th>
+                    <th scope="col"><td>Consommation:</td> <td><?php echo ' '.$kwh.' Kwh';?></td></th>
+                </tr>
+            </thead>
+        </table></div>
+
+        <section class="">
+            
+            <div class="container">
+              <div class="row">
+                <div class="col">
+
+        <!-- col1 -->
+        <div class="pt-5 mt-3">
+          CONSOMMATION ELECTRICITE<br>
+          <tr> <td class="text-secondary small "><?php echo $tranche;?></td></tr><br>
+        <!-- <td class="text-secondary small pt-1">TRANCHE 2</td> -->
+        <tr> <td class="py-2 ">REDEVANCE FIXE ELECTRICITE</td></tr><br>
+        <tr>  <td class="pb-1">TAXES POUR LE COMPTE DE L’ETAT</td></tr><br>
+        <tr> <td class="text-secondary small pt-1">TOTAL TVA</td></tr><br>
+        <tr> <td class="text-secondary small pt-2 ">TIMBRE</td></tr><br>
+        <tr> <td class="pt-1">SOUS-TOTAL</td></tr><br>
+        <tr> <td>TOTAL ÉLECTRICITÉ</td></tr><br>
+    </div></div><br>
+
+    <!-- col2 -->
+  
+
+    <div class="col-6"><table  class="table text-center border-white ">
+        <thead class="">
+          <tr>
+            <th scope="col">مفوتر<br>Facturé</th>
+            <th scope="col">س.و<br>P.U</th>
+            <th scope="col">المبلغ د.إ.ر<br>Montant HT</th>
+            <th scope="col">ض.ق.م<br>Taux TVA</th>
+                <th>مبلغ الرسوم<br>Montant Taxes</th>
+          </tr>
+        </thead>
+         <tbody class="border-white"><tr>
+            <td><?php echo $kwh;?></td>
+            <td><?php echo $P_U;?></td>
+            <td><?php echo number_format($montantht, 2, ',', ' ');?></td>
+            <td class="pt-3"><?php echo $tva * 100;?>%</td>
+            <td><?php echo number_format($mttaxes, 2, ',', ' ');?></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td><?php echo $caliber;?></td>
+            <td class="pt-0"><?php echo $tva * 100;?>%</td>
+            <td><?php echo $redevancetx;?></td>
+        </tr>
+        <tr><td> </td><td> </td><td> </td><td> </td> <td> </td> </tr>
+        <!-- <tr><td> </td><td> </td><td> </td><td> </td> <td> </td> </tr> -->
+        <tr><td> </td><td> </td><td> </td><td> </td> <td> </td> </tr>
+        <tr><td> </td><td> </td><td> </td><td> </td> <td> </td> </tr>
+        <tr><td> </td><td> </td><td> </td><td> </td> <td> </td> </tr>
+        <tr><td> </td><td> </td><td> </td><td> </td> <td> </td> </tr>
+        <tr><td> </td><td> </td><td> </td><td> </td> <td> <?php echo $timbre;?></td> </tr>
+        </tr>
+    </tbody>
+</table>
+</div>
+
+    <!-- col3 -->
+    <div class="col pt-5 mt-3 text-end">
+
+
+       <tr> <td>إستھلاك الكھرباء</td></tr><br>
+       <tr> <td class="text-secondary small pt-1">1الشطر</td></tr><br>
+        <!-- <td class="text-secondary small pt-1">2الشطر</td> -->
+        <tr> <td class="py-2">إثاوة ثابتة الكھرباء</td></tr><br>
+        <tr> <td class="pb-1"> الرسوم المؤداة لفائدة الدولة </td></tr><br>
+        <tr> <td class="text-secondary small pt-1">مجموع ض.ق.م</td></tr><br>
+        <tr> <td class="text-secondary small pt-2">الطابع</td></tr><br>
+        <tr> <td class="bt-1">المجموع الجزئي</td></tr><br>
+        <tr> <td>
+            مجموع الكھرباء</td>
+        </div>
+              </div>
+            </div>
+        </section>
+    </body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</html>
 
